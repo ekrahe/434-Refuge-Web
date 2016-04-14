db128f7f92211f06d29163db0d69395d4af65ae8
EC2 Default User <ec2-user@ip-172-31-29-214.ec2.internal>
2016-04-14 15:58:45 +0000

    initial commit

---
+++ event_homepage.php
@@ -0,0 +1,7 @@
+<?php
+
+echo '<link href="event_homepage.css" rel="stylesheet">';
+
+echo "<iframe src=\"https://calendar.google.com/calendar/embed?src=f2prj9o0uq3cju3sh9hs67mhik%40group.calendar.google.com&ctz=America/New_York\" style=\"border: 0\" width=\"800\" height=\"600\" frameborder=\"0\" scrolling=\"no\"></iframe>";
+
+?>
