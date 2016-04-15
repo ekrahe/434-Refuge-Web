b7c64d7183aabd3648c3e194964fe0a160e476dc
akiva <akiva.futter@gmail.com>
2016-04-15 13:21:21 -0400

    more changes

--- event_homepage.php
+++ event_homepage.php
@@ -1,12 +1,18 @@
 <?php
 
 echo '<link href="event_homepage.css" rel="stylesheet">';
-echo '<h1 id="header">REFUGE</h1	>';
-echo '<div class=sidebar>';     
+echo '<h1 class="header">REFUGE</h1	>';
+echo '<div class="sidebar">';     
 echo '<p>text</p>';
 
 
 echo '</div>';
-echo "<iframe id=\"cal\" src=\"https://calendar.google.com/calendar/embed?src=f2prj9o0uq3cju3sh9hs67mhik%40group.calendar.google.com&ctz=America/New_York\" style=\"border: 0\" width=\"800\" height=\"600\" frameborder=\"0\" scrolling=\"no\"></iframe>";
+echo '<div class="center">';
+	echo "<iframe class=\"cal\" src=\"https://calendar.google.com/calendar/embed?src=f2prj9o0uq3cju3sh9hs67mhik%40group.calendar.google.com&ctz=America/New_York\" style=\"border: 0\" width=\"800\" height=\"600\" frameborder=\"0\" scrolling=\"no\"></iframe>";
+echo '</div>';
+echo '<div class="event_creator">';
+echo'<p> hello</p>';
+
+echo '</div>';
 
 ?>
