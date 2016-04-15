046c110aca08214fb1746739cb898e5611e460bf
EC2 Default User <ec2-user@ip-172-31-29-214.ec2.internal>
2016-04-15 17:06:43 +0000

    more hompage stuff

--- event_homepage.php
+++ event_homepage.php
@@ -1,7 +1,12 @@
 <?php
 
 echo '<link href="event_homepage.css" rel="stylesheet">';
+echo '<h1 id="header">REFUGE</h1	>';
+echo '<div class=sidebar>';     
+echo '<p>text</p>';
 
-echo "<iframe src=\"https://calendar.google.com/calendar/embed?src=f2prj9o0uq3cju3sh9hs67mhik%40group.calendar.google.com&ctz=America/New_York\" style=\"border: 0\" width=\"800\" height=\"600\" frameborder=\"0\" scrolling=\"no\"></iframe>";
+
+echo '</div>';
+echo "<iframe id=\"cal\" src=\"https://calendar.google.com/calendar/embed?src=f2prj9o0uq3cju3sh9hs67mhik%40group.calendar.google.com&ctz=America/New_York\" style=\"border: 0\" width=\"800\" height=\"600\" frameborder=\"0\" scrolling=\"no\"></iframe>";
 
 ?>
