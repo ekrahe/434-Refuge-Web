74616431788e760b897810f38b2212311ace492c
akiva <akiva.futter@gmail.com>
2016-05-09 16:40:52 -0400

    js changes and buton formatting

--- event_homepage.js
+++ event_homepage.js
@@ -1,8 +1,14 @@
 jQuery(document).ready(function($){
+  $('.eventButton').css('textDecoration', 'none');
   $('.eventButton').on('click', function(e){
   	console.log("got to onclick");
     e.preventDefault();
     $(this).siblings('.eventInfo').toggle();
+  });
+  $('.eventInfo').on('click', function(e){
+  	console.log("got to eventInfo onclick");
+    e.preventDefault();
+    $(this).siblings('.eventInfo').hide();
   });
 
 });
